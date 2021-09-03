6c1b13295f461b45bd4008a5532b9f00c850326c
asmaaraafat27 <89736386+asmaaraafat27@users.noreply.github.com>
2021-09-03 07:28:01 +0200

    Add files via upload

---
+++ modular code.py
@@ -0,0 +1,7 @@
+def favorite_city(name):
+    print("One of my favorite cities is" ,name)
+    
+favorite_city(name = "Alexandria, Egypt")
+favorite_city(name = "Rome, Italy")
+favorite_city(name = "Greece, Thessaloniki")
+favorite_city(name = "Belgium, Brussels")
