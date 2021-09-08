e02dd9c3f0ebee718afed8b370d15aa9a9eb586b
Asmaa <89736386+asmaaraafat27@users.noreply.github.com>
2021-09-08 22:49:04 +0200

    Add files via upload

---
+++ programming fundamental/conditional code.py
@@ -0,0 +1,9 @@
+myfavorite = " Sushi"
+food = input("What's my favorite food?")
+
+if food == myfavorite :
+    print("Yup! so amazing!")
+else :
+    print("Yuck! that's not it!")
+
+print("Thanks for playing!")
