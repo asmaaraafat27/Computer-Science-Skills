33f695b6713b94ac0484ae8a39e5681560334af5
Asmaa <89736386+asmaaraafat27@users.noreply.github.com>
2021-09-08 22:35:21 +0200

    Add files via upload

---
+++ programming fundamentals/modular code.py
@@ -0,0 +1,18 @@
+print("A program by Asmaa")
+
+def asmaa (name):
+    if(name == "Asmaa"):
+      return name 
+    else:
+        print("hmmm, try again")
+
+names = asmaa("Asmaa")
+ 
+if (names == "Asmaa"):
+    print("love")
+else :
+    print("nope")
+    
+    
+#ayhaga = str(input("whats ur name?"))
+    #name = (f"the name is : {name}")
