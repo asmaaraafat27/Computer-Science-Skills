8115902d4dd92d0c7cbbc4d4286fd384ca951271
Asmaa <89736386+asmaaraafat27@users.noreply.github.com>
2021-09-08 22:49:45 +0200

    Delete modular code.py

--- modular code.py
+++
@@ -1,7 +0,0 @@
-def favorite_city(name):
-    print("One of my favorite cities is" ,name)
-    
-favorite_city(name = "Alexandria, Egypt")
-favorite_city(name = "Rome, Italy")
-favorite_city(name = "Greece, Thessaloniki")
-favorite_city(name = "Belgium, Brussels")
