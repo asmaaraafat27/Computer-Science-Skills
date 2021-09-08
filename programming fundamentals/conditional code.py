20a369b7157aea40def1beaecc7a707f364702d3
Asmaa <89736386+asmaaraafat27@users.noreply.github.com>
2021-09-08 22:37:06 +0200

    Delete conditional code.py

--- programming fundamentals/conditional code.py
+++
@@ -1,9 +0,0 @@
-myfavorite = " Sushi"
-food = input("What's my favorite food?")
-
-if food == myfavorite :
-    print("Yup! so amazing!")
-else :
-    print("Yuck! that's not it!")
-
-print("Thanks for playing!")
